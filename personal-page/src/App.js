9af2edd2e7688984e559a8e87765fc5d35e21ba0
github username <github email address>
2020-02-23 17:15:44 -0700

    flipping profile pic

--- personal-page/src/App.js
+++ personal-page/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ProfilePic from './pictures.png/ProfilePic.jpg';
+import profilecard from './components/profilecard'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ProfilePic} className="profile-pic" alt="pic" />
+        <img src={profilecard} className="profile-pic" alt="pic" />
         <p>
           Seth Maisel Puissant Portfolio and Prolific Profile Page
         </p>
---
+++ personal-page/src/components/profilecard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class CardFront extends React.Component {
+    render() {
+        return (
+            <div className='card-side side-front'>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col-xs-6'>
+                            <img src='./pictures.png/ProfilePic' />
+                        </div>
+
+                        <div className='col-xs-6 side-front-content'>
+                            <h2>Czech based</h2>
+
+                            <h1>UI/UX Designer</h1>
+
+                            <p>Andrey is driven by turning ideas into scalable and and empowering experiences that solve real life problems.</p>
+
+                            <p>He is currently the founder of Dvorak Media. Previously, Andrey was a product designer at Dropbox.</p>
+
+                            <p>Over the years, Michael has been priviledged to have worked with Adobe, Evernote, Square and more.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CardFront;
