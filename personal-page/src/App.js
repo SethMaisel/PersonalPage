f775a5998ef6559a34cd8fc3958623ca8ff330bd
github username <github email address>
2020-02-23 23:21:07 -0700

    blogs link added

--- personal-page/src/App.js
+++ personal-page/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProfileCard from './components/ProfileCard.js'
 import ProjectsPage from './components/ProjectsPage.js'
+import Blogs from './components/Blogs.js'
 import './App.css';
 
 function App() {
@@ -17,14 +18,17 @@ function App() {
       <div className='col-xs-6 side-front-content'>
 
         <h1>Full-Stack Developer</h1>
+        <a href="https://medium.com/@seth.maisel">Check out my Blogs!</a>
 
         <p>I have an instinct for the user experience and want to bring my creative flair to the world of coding.</p>
 
         <p>Graduate of the Flatiron School Software Develpment Boot Camp. Ruby on Rails, JavaScript, React</p>
-        
+
         <p>Software Developer, Fight Choreographer, Juggler, Former Raft Guide</p>
+        <ProjectsPage />
+        <Blogs />
       </div>
-      <ProjectsPage />
+
     </div>
   );
 }
---
+++ personal-page/src/components/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Blogs extends React.Component {
+    render() {
+        return (
+         <a href='https://medium.com/@seth.maisel'  /> 
+        )
+    }
+}
+
+export default Blogs;
